0e9c1763a4ff2baf48aa92c9b5b045ff254e3c90
MarquesVivian <60431616+MarquesVivian@users.noreply.github.com>
2020-11-05 13:59:51 +0100

    3

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -124,7 +125,7 @@ class PanierController extends AbstractController
     /**
      * @Route("/panier/add/{id}", name="panier_add")
      */
-    public function add(): Response
+    public function add(SessionInterface ): Response
     {
 
     }
